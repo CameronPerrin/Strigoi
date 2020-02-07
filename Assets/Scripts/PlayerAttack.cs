095e8452efa6fa76b91ff8e6e3187d591bd6248f
Robert Shamus <bshamus630@gmail.com>
2020-02-06 21:51:51 -0500

    Implementation of Attack Circles

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject attackCircle;
+    public GameObject player;
+    private Vector3 playerPos;
+    private Vector3 tempVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            Attack();
+        } 
+    }
+
+    void Attack()
+    {
+        tempVector = new Vector3(playerPos.x+1, playerPos.y, playerPos.z);
+        Instantiate(attackCircle, tempVector, player.transform.rotation);
+    }
+}
