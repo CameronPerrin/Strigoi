e28ad6bf646c2fa03ce2403b0f288c78cbeee387
Robert Shamus <bshamus630@gmail.com>
2020-02-10 20:41:52 -0500

    Added Bricking Scene and Camera Movement

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject camera;
+    private Vector3 playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = new Vector3 (player.transform.position.x, player.transform.position.y, -10);
+        camera.transform.position = playerPos;
+    }
+}
