33b0604886ae1e9fad87f0d45d6531f9cb3b78a6
josh <berightbackstudios@gmail.com>
2020-02-16 22:32:49 -0500

    Added Parallax
    - Added a simple Parallax
    - We still require proper assets to test on

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    
-    public GameObject cam;
-    public float parallaxEffect;
+	// This type of parallax system is using the default camera
+	// if we want to use a differnt camera then we'll need to grab it
+	// through a public object
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool infiniteHorizontal;
+    [SerializeField] private bool infiniteVertical;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
 
-    void Start()
+    private void Start()
     {
-    	startpos = transform.position.x;
-    	length = GetComponent<SpriteRenderer>().bounds.size.x;    
+    	cameraTransform = Camera.main.transform;
+    	lastCameraPosition = cameraTransform.position;
+    	Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+    	Texture2D texture = sprite.texture;
+    	textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+    	textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+    	Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+    	transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+    	lastCameraPosition = cameraTransform.position;
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    	if(infiniteHorizontal)
+    	{
+    		if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+    		{
+    			float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+    			transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+    		}
+    	}
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+    	if(infiniteVertical)
+    	{
+    		if(Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+    		{
+    			float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeX;
+    			transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+    		}
+    	}
     }
 }
