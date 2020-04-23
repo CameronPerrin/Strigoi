58562419e3721b4cfb5d40f5291ec4f2f266fa8f
Cameron Perrin <perrin.cameron@gmail.com>
2020-04-23 19:26:17 -0400

    Animation no longer sticks on spam.
    I gutted PlayerAttackAnimation and made state machine behaviour scripts.
    Next step is to add attack movement to the behaviors

--- Assets/Scripts/PlayerAttackAnimation.cs
+++ Assets/Scripts/PlayerAttackAnimation.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerAttackAnimation : MonoBehaviour
 {
 
-    float clicked = 0;
-    float clicktime = 0;
-    float clickdelay = 0.4f;
-    float doubleDelay;
-    float attackDelay;
-    bool canDo = true;
+    // float clicked = 0;
+    // float clicktime = 0;
+    // float clickdelay = 0.4f;
+    // float doubleDelay;
+    // float attackDelay;
+    // bool canDo = true;
     public Animator playerAnimator;
     //private bool waiting = false;
 
@@ -19,48 +19,54 @@ public class PlayerAttackAnimation : MonoBehaviour
     {
         if(PauseMenu.GameIsPaused == false)
         {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && 
+        	!playerAnimator.GetBool("isAttackDouble") && 
+        	!playerAnimator.GetBool("isAttackLonely") &&
+        	!playerAnimator.GetBool("isAttack"))
         {
             playerAnimator.SetBool("isAttack", true);
-            Debug.Log("Start");
-
+            //Debug.Log("Start");
+/*
             clicked++;
             if (clicked == 1) clicktime = Time.time;
 
             if (clicked == 2 && Time.time - clicktime < clickdelay)
             {
-                playerAnimator.SetBool("isAttackDouble", true);
-                playerAnimator.SetBool("isAttack", false);
+                //playerAnimator.SetBool("isAttackDouble", true);
+                //playerAnimator.SetBool("isAttack", false);
                 clicked = 0;
                 clicktime = 0;
                 doubleDelay = Time.time;
-                Debug.Log("DOUBLE");
+                //Debug.Log("DOUBLE");
 
             }
+
+            */
         }
 
-
+/*
         if ((clicked != 0) && (clicked == 2 || Time.time - clicktime > clickdelay))
         {
-            Debug.Log("SINGLE");
-            playerAnimator.SetBool("isAttackLonely", true);
-            playerAnimator.SetBool("isAttack", false);
+            //Debug.Log("SINGLE");
+            //playerAnimator.SetBool("isAttackLonely", true);
+            //playerAnimator.SetBool("isAttack", false);
 
             clicked = 0;
         }
 
         if ((playerAnimator.GetBool("isAttackLonely")) && (Time.time - clicktime > 0.7f))
         {
-            playerAnimator.SetBool("isAttackLonely", false);
-            playerAnimator.SetBool("isAttack", false);
+            //playerAnimator.SetBool("isAttackLonely", false);
+            //playerAnimator.SetBool("isAttack", false);
         }
 
         if (playerAnimator.GetBool("isAttackDouble") && (Time.time - doubleDelay > 0.3f))
         {
-            playerAnimator.SetBool("isAttackDouble", false);
-            playerAnimator.SetBool("isAttack", false);
+            //playerAnimator.SetBool("isAttackDouble", false);
+            //playerAnimator.SetBool("isAttack", false);
             doubleDelay = 0;
         }
+
 
 
         //stop all movement and flipping
@@ -87,6 +93,9 @@ public class PlayerAttackAnimation : MonoBehaviour
             GetComponent<Player>().moveSpeed = 7;
             GetComponent<Player>().canFlip = true;
         }
+        */
     }
+
     }
+
 }
