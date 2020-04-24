67787bcb8a492fda33b8f3fa11bcdaded9cb02da
Cameron Perrin <perrin.cameron@gmail.com>
2020-04-23 20:37:56 -0400

    Fixed anims again and then added some things to the scene for itchio video

--- Assets/AttackDoubleBehavior.cs
+++ Assets/AttackDoubleBehavior.cs
@@ -23,6 +23,7 @@ public class AttackDoubleBehavior : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+    animator.SetBool("isAttackLonely", false);
        animator.SetBool("isAttackDouble", false);
     }
 
--- Assets/AttackSingleBehavior.cs
+++ Assets/AttackSingleBehavior.cs
@@ -21,6 +21,7 @@ public class AttackSingleBehavior : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        animator.SetBool("isAttackLonely", false);
+       animator.SetBool("isAttackDouble", false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
